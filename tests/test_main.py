4f0151447cb0193daaef5414a852be45fa9ab9b5
Office L <nishimuray@gmail.com>
2016-12-15 18:57:34 +0900

    #31 all.dic open?

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,16 @@
+import os
 import unittest
 
 
-class TestMain(unittest.TestCase):
+class TestAllDirectory(unittest.TestCase):
+
+    def setUp(self):
+        root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+        self.f = open(os.path.abspath(os.path.join(root_path, "all", "all.dic")), "wt", encoding="utf-8", newline="\n")
+
+    def tearDown(self):
+        self.f.close()
+
     def test_all(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
