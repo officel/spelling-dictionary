ae3084ebe8e5162db74b0046fbe9bf3110420eb8
Office L <nishimuray@gmail.com>
2016-12-15 18:59:27 +0900

    #31 miss all.dic...

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ class TestAllDirectory(unittest.TestCase):
 
     def setUp(self):
         root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-        self.f = open(os.path.abspath(os.path.join(root_path, "all", "all.dic")), "wt", encoding="utf-8", newline="\n")
+        self.f = open(os.path.abspath(os.path.join(root_path, "all", "all.dic")), "rt", encoding="utf-8", newline="\n")
 
     def tearDown(self):
         self.f.close()
